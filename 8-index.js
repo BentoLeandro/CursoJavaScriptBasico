0f9b8df80a96da96ad83d96da7c133376618c1a6
-v <leo_lrb90@hotmail.com>
2021-08-05 21:32:16 -0300

    salvando os novos arquivos

--- 8-index.js
+++ 8-index.js
@@ -1,22 +1,20 @@
-class Cliente {
-    nome;
-    cpf;
-    agencia;
-    saldo;
-}
 
 const cliente1 = new Cliente;
-const cliente2 = new Cliente;
-
 cliente1.nome = "Maria";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-cliente2.nome = "Leandro";
-cliente2.cpf = 99922233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+const contaCorrenteMaria = new ContaCorrente();
+contaCorrenteMaria.agencia = 1001;
+//contaCorrenteMaria.saldo   = 0;
 
-console.log(cliente1);
-console.log(cliente2);
+contaCorrenteMaria.depositar(100);
+const valorSacado = contaCorrenteMaria.sacar(50);
+console.log(valorSacado);
+contaCorrenteMaria.depositar(200);
+contaCorrenteMaria.depositar(-1);
+
+
+
+console.log(contaCorrenteMaria);
+//console.log(cliente1);
+//console.log(cliente2);
