cb51a849f0601b765d2697da68c730fff570df1b
-v <leo_lrb90@hotmail.com>
2021-08-12 23:02:33 -0300

    salvando novos arquivos

--- 8-index.js
+++ 8-index.js
@@ -1,5 +1,7 @@
 import {Cliente} from "./9-Cliente.js"
 import {ContaCorrente} from "./10-ContaCorrente.js"
+import {ContaPoupanca} from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 
 //const cliente1 = new Cliente;
@@ -7,22 +9,20 @@ import {ContaCorrente} from "./10-ContaCorrente.js"
 //cliente1.cpf = 11122233309;
 
 const cliente1 = new Cliente("Maria", 11122233309);
-const cliente2 = new Cliente("Leandro", 99922233309);
+//const cliente2 = new Cliente("Leandro", 99922233309);
 
 
-const contaCorrenteMaria = new ContaCorrente(1001, cliente1);
+const contaCorrenteMaria = new Conta(0, cliente1, 1001);
 contaCorrenteMaria.depositar(500);
+contaCorrenteMaria.sacar(100);
 
-const contaCorrenteLeandro = new ContaCorrente(1002, cliente2);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-contaCorrenteMaria.transferir(200, contaCorrenteLeandro);
+//const contaCorrenteLeandro = new ContaCorrente(1002, cliente2);
 
+//contaCorrenteMaria.transferir(200, contaCorrenteLeandro);
 
-
-
-
+console.log(contaPoupanca);
 console.log(contaCorrenteMaria);
-console.log(contaCorrenteLeandro);
-console.log(ContaCorrente.numeroDeContas);
-//console.log(cliente1);
-//console.log(cliente2);
+//console.log(contaCorrenteLeandro);
+//console.log(ContaCorrente.numeroDeContas);
